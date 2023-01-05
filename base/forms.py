3bc7634fd57823653946ea094837b1769b93acee
P-Horus <feranmia51@gmail.com>
2023-01-05 02:32:25 +0100

    Worked on Adding New Skills page

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Project, Message
+from .models import Project, Message, Skill
 
 
 class ProjectForm(ModelForm):
@@ -32,4 +32,19 @@ class MessageForm(ModelForm):
             {'class':'form-control'})
         self.fields['body'].widget.attrs.update(
             {'class':'form-control'})
+        
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+        
+        
+    def __init__(self, *args, **kwargs):
+        super(SkillForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update(
+            {'class':'form-control'})
+        self.fields['body'].widget.attrs.update(
+            {'class':'form-control'})
+
         
