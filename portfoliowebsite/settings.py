c508fbf7f2a22174d1339cb4465bc4ec24bcf4a8
P-Horus <feranmia51@gmail.com>
2023-02-21 15:03:22 +0100

    Fixed ck_editor

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_PORTFOLIOWEBSITE')
+SECRET_KEY = '0cce6c9ed8f5cb17ec651f200f6ce346'
+# SECRET_KEY = os.environ.get('SECRET_KEY_PORTFOLIOWEBSITE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
