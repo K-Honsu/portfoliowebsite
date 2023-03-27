1b3fd0e990019fff5e92ad1735b70a429d3c6d9b
P-Horus <feranmia51@gmail.com>
2023-03-27 17:59:14 +0100

    updated file

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0cce6c9ed8f5cb17ec651f200f6ce346'
-# SECRET_KEY = os.environ.get('SECRET_KEY_PORTFOLIOWEBSITE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
